f3f85fd23d8492bdfb877ef3ca01447078aba481
WesleyCKY <wesleychin9966@gmail.com>
2021-07-20 16:41:13 +0800

    improve and simplify a switch function

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ class RangeSlider {
         this.numTicks = this.maxValue / this.minValue;
         this.input.addEventListener('change', (e) => this.handleInputChange(e));
 
-        switchBackGround(0);
+        switchBackGround("0");
 
         this.wrapInput().then(wrapperId => {
             if (this.input.hasAttribute('values')) {
@@ -142,72 +142,73 @@ function switchLink(value) {
 
 
 function switchBackGround(value) {
-    let text = '';
     let link = '';
-
-    // console.log("Value handled..");
-    if (value == 0) {
-        let backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/xizhou/xizhou/xizhou_02.png)";
-        document.body.style.backgroundImage = backgroundImg;
-    } else if (value == 1) {
-        let backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/chunqiu/chunqiu/chunqiu_02.png)";
-        document.body.style.backgroundImage = backgroundImg;
-    } else if (value == 2) {
-        let backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/zhanguo/zhanguo/zhanguo_02.png)";
-        document.body.style.backgroundImage = backgroundImg;
-    } else if (value == 3) {
-        let backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/qin/qin/qin_02.png)";
-        document.body.style.backgroundImage = backgroundImg;
-    } else if (value == 4) {
-        let backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/xihan/xihan/xihan_02.png)";
-        document.body.style.backgroundImage = backgroundImg;
-    } else if (value == 5) {
-        let backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/donghan/donghan/donghan_02.png)";
-        document.body.style.backgroundImage = backgroundImg;
-    } else if (value == 6) {
-        let backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/sanguo/sanguo/sanguo_02.png)";
-        document.body.style.backgroundImage = backgroundImg;
-    } else if (value == 7) {
-        let backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/xijin/xijin/xijin_02.png)";
-        document.body.style.backgroundImage = backgroundImg;
-    } else if (value == 8) {
-        let backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/dongjin/dongjin/dongjin_02.png)";
-        document.body.style.backgroundImage = backgroundImg;
-    } else if (value == 9) {
-        let backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/nanbeichao/nanbeichao/nanbeichao.png)";
-        document.body.style.backgroundImage = backgroundImg;
-    } else if (value == 10) {
-        let backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/sui/sui/sui_02.png)";
-        document.body.style.backgroundImage = backgroundImg;
-    } else if (value == 11) {
-        let backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/tang/tang/tang_02.png)";
-        document.body.style.backgroundImage = backgroundImg;
-    } else if (value == 12) {
-        let backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/beisong/beisong/beisong_02.png)";
-        document.body.style.backgroundImage = backgroundImg;
-    } else if (value == 13) {
-        let backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/nansong/nansong/nansong_02.png)";
-        document.body.style.backgroundImage = backgroundImg;
-    } else if (value == 14) {
-        let backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/yuan/yuan/yuan_02.png)";
-        document.body.style.backgroundImage = backgroundImg;
-    } else if (value == 15) {
-        let backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/ming/ming/ming_02.png)";
-        document.body.style.backgroundImage = backgroundImg;
-    } else if (value == 16) {
-        let backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/qing/qing/qing_02.png)";
-        document.body.style.backgroundImage = backgroundImg;
-    } else if (value == 17) {
-        let backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/zhonghuaminguo/zhonghuaminguo/zhonghuaminguo_02.png)";
-        document.body.style.backgroundImage = backgroundImg;
-    } else if (value == 18) {
-        let backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/zhongguo/zhongguo/zhongguo.png)";
-        document.body.style.backgroundImage = backgroundImg;
-    } else {
-        console.log("else in switchBackGround...");
+    let backgroundImg = "";
+    switch (value) {
+        case "0":
+            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/xizhou/xizhou/xizhou_02.png)";
+            break;
+        case "1":
+            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/chunqiu/chunqiu/chunqiu_02.png)";
+            break;
+        case "2":
+            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/zhanguo/zhanguo/zhanguo_02.png)";
+            break;
+        case "3":
+            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/qin/qin/qin_02.png)";
+            break;
+        case "4":
+            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/xihan/xihan/xihan_02.png)";
+            break;
+        case "5":
+            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/donghan/donghan/donghan_02.png)";
+            break;
+        case "6":
+            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/sanguo/sanguo/sanguo_02.png)";
+            break;
+        case "7":
+            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/xijin/xijin/xijin_02.png)";
+            break;
+        case "8":
+            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/dongjin/dongjin/dongjin_02.png)";
+            break;
+        case "9":
+            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/nanbeichao/nanbeichao/nanbeichao.png)";
+            break;
+        case "10":
+            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/sui/sui/sui_02.png)";
+            break;
+        case "11":
+            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/tang/tang/tang_02.png)";
+            break;
+        case "12":
+            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/beisong/beisong/beisong_02.png)";
+            break;
+        case "13":
+            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/nansong/nansong/nansong_02.png)";
+            break;
+        case "14":
+            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/yuan/yuan/yuan_02.png)";
+            break;
+        case "15":
+            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/ming/ming/ming_02.png)";
+            break;
+        case "16":
+            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/qing/qing/qing_02.png)";
+            break;
+        case "17":
+            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/zhonghuaminguo/zhonghuaminguo/zhonghuaminguo_02.png)";
+            break;
+        case "18":
+            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/zhongguo/zhongguo/zhongguo.png)";
+            break;
+        default:
+            break;
     }
+    document.body.style.backgroundImage = backgroundImg;
     return link;
 }
+
 
 function preloadImages(array) {
     if (!preloadImages.list) {

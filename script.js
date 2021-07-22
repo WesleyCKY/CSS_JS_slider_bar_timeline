bea0c3c1f2ede9d778abef3655c0741e583bdfc9
WesleyCKY <wesleychin9966@gmail.com>
2021-07-22 14:20:40 +0800

    timeline spot display in time scale, change text style

--- script.js
+++ script.js
@@ -1,114 +1,24 @@
-const createUniqueId = prefix => {
-    return `${prefix}-${Math.round(Math.random() * 100000)}`;
-}
-
-class RangeSlider {
-
-    constructor(el) {
-        this.input = document.querySelector(el);
-        this.el = this.input.getAttribute('id');
-        this.minValue = this.input.getAttribute('min');
-        this.maxValue = this.input.getAttribute('max');
-        this.numTicks = this.maxValue / this.minValue;
-        this.input.addEventListener('change', (e) => this.handleInputChange(e));
-
-        switchBackGround("0");
-
-        this.wrapInput().then(wrapperId => {
-            if (this.input.hasAttribute('values')) {
-                this.values = JSON.parse(this.input.getAttribute('values'));
-                this.createTicks(wrapperId);
-            }
+const SLIDER_CONTAINER = ".slider";
+const SLIDER_RANGE_INPUT = "input[type='range']";
+let link = '';
+// custom sliding bar
+document.querySelectorAll(".__range-step").forEach(function(ctrl) {
+    var el = ctrl.querySelector('input');
+    var output = ctrl.querySelector('output');
+    var newPoint, newPlace, offset;
+    el.oninput = function() {
+        // colorize step options
+        ctrl.querySelectorAll("option").forEach(function(opt) {
+            if (opt.value <= el.valueAsNumber)
+                opt.style.backgroundColor = '#48530d';
+            else
+                opt.style.backgroundColor = '#a4b162';
         });
-
-    }
-
-    handleInputChange(e) {
-        const value = e.target.value;
-        const ariaValueText = !this.values ? value : this.values[value];
-        this.input.setAttribute('aria-valuetext', ariaValueText);
-        this.setSelectedLabel(this.values[value]);
-        switchBackGround(value);
-    }
-
-    handleLabelClick(label, value, e) {
-        this.input.focus();
-        this.input.value = value;
-        this.input.setAttribute('value', value);
-        this.input.setAttribute('aria-valuetext', label);
-        this.setSelectedLabel(label);
-        switchBackGround(value);
-
-    }
-
-    setSelectedLabel(label) {
-        const selectedLabels = this.input.parentNode.querySelectorAll(`.range-slider-ticks2__label`);
-        [].forEach.call(selectedLabels, el => {
-            el.classList[el.innerText === label ? 'add' : 'remove']('is-selected');
-        });
-    }
-
-    wrapInput() {
-        return new Promise(resolve => {
-            const wrapper = document.createElement('div');
-            const wrapperId = createUniqueId('range-slider2');
-            wrapper.id = wrapperId;
-            wrapper.className = 'range-slider2';
-            this.input.parentNode.replaceChild(wrapper, this.input);
-            document.querySelector(`#${wrapperId}`).appendChild(this.input);
-            resolve(wrapperId);
-        });
-    }
-
-    createTicks(wrapperId) {
-        return new Promise(resolve => {
-            let index = 0;
-            let tickLabelText;
-            const tickList = document.createElement('div');
-            const tickListId = createUniqueId('tick-list');
-            const noLabels = this.input.hasAttribute('no-labels');
-            const firstAndLastLabelsOnly = this.input.hasAttribute('first-last-labels-only');
-            tickList.id = tickListId;
-            tickList.className = 'range-slider-ticks2';
-            document.querySelector(`#${wrapperId}`).appendChild(tickList);
-
-            for (const prop in this.values) {
-                if (this.values.hasOwnProperty(prop)) {
-                    const isFirstOrLastLabel = !!firstAndLastLabelsOnly &&
-                        index > 0 &&
-                        index < Object.values(this.values).length - 1;
-
-                    const tick = document.createElement('div');
-                    const tickLabel = document.createElement('span');
-                    tickLabel.className = `range-slider-ticks2__label ${this.input.value === prop ? 'is-selected' : ''}`;
-
-                    if (!noLabels) {
-                        tickLabelText = document.createTextNode(
-                            isFirstOrLastLabel ? '' : this.values[prop]
-                        );
-                        var a = document.createElement('a');
-                        a.href = switchLink(index);
-                        var linkText = tickLabelText;
-                        a.appendChild(linkText);
-                        tickLabel.appendChild(a);
-                    }
-
-                    tick.className = 'range-slider-ticks2__tick';
-                    tick.addEventListener('click', (e) => this.handleLabelClick(this.values[prop], prop, e));
-                    tick.appendChild(tickLabel);
-                    document.querySelector(`#${tickListId}`).appendChild(tick);
-                    index += 1;
-                }
-            }
-            resolve();
-        });
-    }
-}
-
-[].forEach.call(document.querySelectorAll('input[type="range"]'), el => {
-    const id = `#${el.getAttribute('id')}`;
-    return new RangeSlider(id);
+    };
+    el.oninput();
 });
+
+
 
 /** Initialize sliders and listen for value changes. */
 function listenForSliderChanges() {
@@ -129,86 +39,125 @@ function listenForSliderChanges() {
     });
 }
 
-function switchLink(value) {
-    var link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty';
-    if (value < 10) {
-        link += '0' + value;
-        console.log(link);
-    } else {
-        link += value;
-    }
-    return link;
-}
+/** Update text value of a given container based on a range input. */
+function updateSliderValue($input, $valueContainer) {
+    let value = $input.val();
+    const min = $input.prop("min");
+    const max = $input.prop("max");
+    let text = '';
 
-
-function switchBackGround(value) {
-    let link = '';
     let backgroundImg = "";
-    switch (value) {
-        case "0":
-            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/xizhou/xizhou/xizhou_02.png)";
-            break;
-        case "1":
-            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/chunqiu/chunqiu/chunqiu_02.png)";
-            break;
-        case "2":
-            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/zhanguo/zhanguo/zhanguo_02.png)";
-            break;
-        case "3":
-            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/qin/qin/qin_02.png)";
-            break;
-        case "4":
-            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/xihan/xihan/xihan_02.png)";
-            break;
-        case "5":
-            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/donghan/donghan/donghan_02.png)";
-            break;
-        case "6":
-            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/sanguo/sanguo/sanguo_02.png)";
-            break;
-        case "7":
-            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/xijin/xijin/xijin_02.png)";
-            break;
-        case "8":
-            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/dongjin/dongjin/dongjin_02.png)";
-            break;
-        case "9":
-            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/nanbeichao/nanbeichao/nanbeichao.png)";
-            break;
-        case "10":
-            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/sui/sui/sui_02.png)";
-            break;
-        case "11":
-            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/tang/tang/tang_02.png)";
-            break;
-        case "12":
-            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/beisong/beisong/beisong_02.png)";
-            break;
-        case "13":
-            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/nansong/nansong/nansong_02.png)";
-            break;
-        case "14":
-            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/yuan/yuan/yuan_02.png)";
-            break;
-        case "15":
-            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/ming/ming/ming_02.png)";
-            break;
-        case "16":
-            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/qing/qing/qing_02.png)";
-            break;
-        case "17":
-            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/zhonghuaminguo/zhonghuaminguo/zhonghuaminguo_02.png)";
-            break;
-        case "18":
-            backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/zhongguo/zhongguo/zhongguo.png)";
-            break;
-        default:
-            break;
+    // console.log(value);
+    if (value <= -771) {
+        text = '西周';
+        backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/xizhou/xizhou/xizhou_02.png)";
+        link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty01';
+    } else if (value >= -770 && value <= -404) {
+        text = '春秋';
+        backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/chunqiu/chunqiu/chunqiu_02.png)";
+        link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty02';
+    } else if (value >= -403 && value <= -222) {
+        text = '戰國';
+        backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/zhanguo/zhanguo/zhanguo_02.png)";
+        link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty03';
+    } else if (value >= -221 && value <= -203) {
+        text = '秦代';
+        backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/qin/qin/qin_02.png)";
+        link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty04';
+    } else if (value >= -202 && value <= 24) {
+        text = '西漢';
+        backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/xihan/xihan/xihan_02.png)";
+        link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty05';
+    } else if (value >= 25 && value <= 219) {
+        text = '東漢';
+        backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/donghan/donghan/donghan_02.png)";
+        link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty06';
+    } else if (value >= 220 && value <= 264) {
+        text = '三國';
+        backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/sanguo/sanguo/sanguo_02.png)";
+        link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty07';
+    } else if (value >= 265 && value <= 316) {
+        text = '西晉';
+        backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/xijin/xijin/xijin_02.png)";
+        link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty08';
+    } else if (value >= 317 && value <= 419) {
+        text = '東晉';
+        backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/dongjin/dongjin/dongjin_02.png)";
+        link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty09';
+    } else if (value >= 420 && value <= 580) {
+        text = '南北朝';
+        backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/nanbeichao/nanbeichao/nanbeichao.png)";
+        link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty10';
+    } else if (value >= 581 && value <= 617) {
+        text = '隋代';
+        backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/sui/sui/sui_02.png)";
+        link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty11';
+    } else if (value >= 618 && value <= 959) {
+        text = '唐代';
+        backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/tang/tang/tang_02.png)";
+        link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty12';
+    } else if (value >= 960 && value <= 1126) {
+        text = '北宋';
+        backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/beisong/beisong/beisong_02.png)";
+        link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty13';
+    } else if (value >= 1127 && value <= 1270) {
+        text = '南宋';
+        backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/nansong/nansong/nansong_02.png)";
+        link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty14';
+    } else if (value >= 1271 && value <= 1367) {
+        text = '元代';
+        backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/yuan/yuan/yuan_02.png)";
+        link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty15';
+    } else if (value >= 1368 && value <= 1643) {
+        text = '明代';
+        backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/ming/ming/ming_02.png)";
+        link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty16';
+    } else if (value >= 1644 && value <= 1911) {
+        text = '清代';
+        backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/qing/qing/qing_02.png)";
+        link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty17';
+    } else if (value >= 1912 && value <= 1948) {
+        text = '中華民國';
+        backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/zhonghuaminguo/zhonghuaminguo/zhonghuaminguo_02.png)";
+        link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty18';
+    } else if (value >= 1949 && value <= 2021) {
+        text = '中華人民共和國';
+        backgroundImg = "url(https://wesleycky.github.io/ARWeb/assets/png/map/zhongguo/zhongguo/zhongguo.png)";
+        link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty19';
+    } else {
+        text = "";
     }
     document.body.style.backgroundImage = backgroundImg;
-    return link;
-}
+    // Clamp within 0 - 100
+    const distanceFromLeft = ((value - min) * 100) / (max - min);
 
+    // $valueContainer.div.appendChild(link);
+    // Contain value within container bounds
+    if (value < 0) {
+        $valueContainer.text(`${text} 前${value * -1}`).css({
+            left: `calc(${distanceFromLeft}%)`,
+            transform: `translateX(-${distanceFromLeft}%)`,
+            marginTop: `-2.6rem`,
+            fontSize: `20px`,
+            bottom: `100%`,
+            textAlign: `left`,
+        });
+    } else {
+        $valueContainer.text(`${text} ${value}`).css({
+            left: `calc(${distanceFromLeft}%)`,
+            transform: `translateX(-${distanceFromLeft}%)`,
+            marginTop: `-2.6rem`,
+            fontSize: `20px`,
+            bottom: `100%`,
+            textAlign: `left`,
+        });
+    }
+
+
+
+
+    // $valueContainer.contents().wrap('<a href=\"' + link + '\"></a>');
+}
 
 function preloadImages(array) {
     if (!preloadImages.list) {
@@ -228,6 +177,7 @@ function preloadImages(array) {
         list.push(img);
         img.src = array[i];
     }
+    console.log("Pre-loaded");
 }
 
 // replace the img cache src...
@@ -252,3 +202,10 @@ preloadImages([
     "https://wesleycky.github.io/ARWeb/assets/png/map/zhonghuaminguo/zhonghuaminguo/zhonghuaminguo_02.png",
     "https://wesleycky.github.io/ARWeb/assets/png/map/zhongguo/zhongguo/zhongguo.png"
 ]);
+// link to other source when clicked on empty sapce of body 
+document.body.onclick = function(e) {
+    if (e.target === document.body) {
+        window.location = link;
+    }
+}
+listenForSliderChanges();

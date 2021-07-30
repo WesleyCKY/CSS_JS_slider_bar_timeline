293f7186cd4d9ec33dfac2693cfa8f670e08f26a
WesleyCKY <wesleychin9966@gmail.com>
2021-07-30 14:44:25 +0800

    change the shape of marker, opacity of input box

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ var tempVal = 2021;
 
 /** Initialize sliders and listen for value changes. */
 function listenForSliderChanges() {
-    console.log("listenForSliderChanges()...");
+    // console.log("listenForSliderChanges()...");
     $(SLIDER_CONTAINER).each(function() {
         // Prepend value container
         $valueContainer = $("<div />");
@@ -39,7 +39,7 @@ function updateValueContainer(value, min, max, $valueContainer, text) {
     const distanceFromLeft = ((value - min) * 100) / (max - min);
 
     if (value < 0) {
-        console.log("(value < 0): ", value);
+        // console.log("(value < 0): ", value);
         $valueContainer.text(`${text}\n 前${-value}`).css({
             width: `7rem`,
             whiteSpace: `pre-line`,
@@ -94,79 +94,79 @@ function checkDynasty(value, text, backgroundImg) {
     // value is ranged based on historical year
     if (value <= -771) {
         text = '西周';
-        backgroundImg = "url(http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/xizhou/xizhou/xizhou_02.png)";
+        backgroundImg = "url(img/xizhou_0.png)";
         link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty01';
     } else if (value >= -770 && value <= -404) {
         text = '春秋';
-        backgroundImg = "url(http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/chunqiu/chunqiu/chunqiu_02.png)";
+        backgroundImg = "url(img/chunqiu_0.png)";
         link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty02';
     } else if (value >= -403 && value <= -222) {
         text = '戰國';
-        backgroundImg = "url(http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/zhanguo/zhanguo/zhanguo_02.png)";
+        backgroundImg = "url(img/zhanguo_0.png)";
         link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty03';
     } else if (value >= -221 && value <= -203) {
         text = '秦代';
-        backgroundImg = "url(http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/qin/qin/qin_02.png)";
+        backgroundImg = "url(img/qin.png)";
         link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty04';
     } else if (value >= -202 && value <= 24) {
         text = '西漢';
-        backgroundImg = "url(http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/xihan/xihan/xihan_02.png)";
+        backgroundImg = "url(img/xihan.png)";
         link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty05';
     } else if (value >= 25 && value <= 219) {
         text = '東漢';
-        backgroundImg = "url(http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/donghan/donghan/donghan_02.png)";
+        backgroundImg = "url(img/donghan.png)";
         link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty06';
     } else if (value >= 220 && value <= 264) {
         text = '三國';
-        backgroundImg = "url(http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/sanguo/sanguo/sanguo_02.png)";
+        backgroundImg = "url(img/sanguo.png)";
         link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty07';
     } else if (value >= 265 && value <= 316) {
         text = '西晉';
-        backgroundImg = "url(http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/xijin/xijin/xijin_02.png)";
+        backgroundImg = "url(img/xijin.png)";
         link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty08';
     } else if (value >= 317 && value <= 419) {
         text = '東晉';
-        backgroundImg = "url(http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/dongjin/dongjin/dongjin_02.png)";
+        backgroundImg = "url(img/dongjin.png)";
         link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty09';
     } else if (value >= 420 && value <= 580) {
         text = '南北朝';
-        backgroundImg = "url(http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/nanbeichao/nanbeichao/nanbeichao.png)";
+        backgroundImg = "url(img/nanbeichao.png)";
         link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty10';
     } else if (value >= 581 && value <= 617) {
         text = '隋代';
-        backgroundImg = "url(http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/sui/sui/sui_02.png)";
+        backgroundImg = "url(img/sui.png)";
         link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty11';
     } else if (value >= 618 && value <= 959) {
         text = '唐代';
-        backgroundImg = "url(http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/tang/tang/tang_02.png)";
+        backgroundImg = "url(img/tang.png)";
         link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty12';
     } else if (value >= 960 && value <= 1126) {
         text = '北宋';
-        backgroundImg = "url(http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/beisong/beisong/beisong_02.png)";
+        backgroundImg = "url(img/beisong.png)";
         link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty13';
     } else if (value >= 1127 && value <= 1270) {
         text = '南宋';
-        backgroundImg = "url(http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/nansong/nansong/nansong_02.png)";
+        backgroundImg = "url(img/nansong.png)";
         link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty14';
     } else if (value >= 1271 && value <= 1367) {
         text = '元代';
-        backgroundImg = "url(http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/yuan/yuan/yuan_02.png)";
+        backgroundImg = "url(img/yuan.png)";
         link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty15';
     } else if (value >= 1368 && value <= 1643) {
         text = '明代';
-        backgroundImg = "url(http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/ming/ming/ming_02.png)";
+        backgroundImg = "url(img/ming.png)";
         link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty16';
     } else if (value >= 1644 && value <= 1911) {
         text = '清代';
-        backgroundImg = "url(http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/qing/qing/qing_02.png)";
+        backgroundImg = "url(img/qing.png)";
         link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty17';
     } else if (value >= 1912 && value <= 1948) {
         text = '中華民國';
-        backgroundImg = "url(http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/zhonghuaminguo/zhonghuaminguo/zhonghuaminguo_02.png)";
+        backgroundImg = "url(img/zhonghuaminguo.png)";
         link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty18';
     } else if (value >= 1949 && value <= 2021) {
         text = '中華人民共和國';
-        backgroundImg = "url(http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/zhongguo/zhongguo/zhongguo.png)";
+        backgroundImg = "url(img/zhongguo.png)";
         link = 'http://eduarapp.iotsolution.hk/collection/tc/dynasty19';
     } else {
         text = "不符合";
@@ -198,39 +198,34 @@ function preloadImages(array) {
         var img = new Image();
         img.onload = function() {
             var index = list.indexOf(this);
-            if (index !== -1) {
-                // remove image from the array once it's loaded
-                // for memory consumption reasons
-                list.splice(index, 1);
-            }
         }
         list.push(img);
         img.src = array[i];
     }
-    console.log("Pre-loaded");
+    // console.log("Pre-loaded");
 }
 
 // replace the img cache src...
 preloadImages([
-    "http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/xizhou/xizhou/xizhou_02.png",
-    "http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/chunqiu/chunqiu/chunqiu_02.png",
-    "http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/zhanguo/zhanguo/zhanguo_02.png",
-    "http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/qin/qin/qin_02.png",
-    "http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/xihan/xihan/xihan_02.png",
-    "http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/donghan/donghan/donghan_02.png",
-    "http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/sanguo/sanguo/sanguo_02.png",
-    "http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/xijin/xijin/xijin_02.png",
-    "http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/dongjin/dongjin/dongjin_02.png",
-    "http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/nanbeichao/nanbeichao/nanbeichao.png",
-    "http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/sui/sui/sui_02.png",
-    "http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/tang/tang/tang_02.png",
-    "http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/beisong/beisong/beisong_02.png",
-    "http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/nansong/nansong/nansong_02.png",
-    "http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/yuan/yuan/yuan_02.png",
-    "http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/ming/ming/ming_02.png",
-    "http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/qing/qing/qing_02.png",
-    "http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/zhonghuaminguo/zhonghuaminguo/zhonghuaminguo_02.png",
-    "http://eduarapp.iotsolution.hk/tpl/ARWeb/assets/png/map/zhongguo/zhongguo/zhongguo.png"
+    "img/beisong.png",
+    "img/chunqiu_0.png",
+    "img/donghan.png",
+    "img/dongjin.png",
+    "img/ming.png",
+    "img/nanbeichao.png",
+    "imgnansong.png",
+    "img/qin.png",
+    "img/qing.png",
+    "img/sanguo.png",
+    "img/sui.png",
+    "img/tang.png",
+    "img/xihan.png",
+    "img/xijin.png",
+    "img/xizhou_0.png",
+    "img/yuan.png",
+    "img/zhanguo_0.png",
+    "img/zhongguo.png",
+    "img/zhonghuaminguo.png",
 ]);
 
 // link to other source when clicked on empty sapce of body 
